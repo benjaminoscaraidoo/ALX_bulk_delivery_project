d36be3ce90808d237c6bc2392bb16db09efd8542
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2026-03-01 22:40:54 +0300

    updated settings py, commented out static path

--- bulk_delivery_project/settings.py
+++ bulk_delivery_project/settings.py
@@ -181,6 +181,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = ['static/']
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
